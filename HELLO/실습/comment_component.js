278ffa022a118fb133750993e9af8084ef3055a5
KRHDY <1901322@g.yju.ac.kr>
2021-07-16 16:50:30 +0900

    메메롱

--- HELLO/실습/comment_component.js
+++ HELLO/실습/comment_component.js
@@ -1,34 +1,35 @@
 Vue.component('comments',{
     
-    template:'', 
-    data: function(){
-        return {
-
+    template:` <div>
+    <input type="text" v-model="user">
+    <input type="text" v-model="comment">
+    <button @click="save">저장</button><br>
+    <ul>
+        <li v-for="msg in messages">
+            <p>{{ msg.user}} : {{msg.comment}}</p>
+        </li>
+    </ul>
+    </div>`, 
+    data(){
+        return { 
+            user : null,
+            comment : null, 
+            messages:[],
         }
+    },
+    methods:{
+            save(){
+                const message= {
+                    user:this.user,
+                    comment:this.comment,
+                };
+                // 내림차순 unshift
+                this.messages.unshift(message)
+                this.user=null
+                this.comment=null
+            }
     }
-}
-
-
-
-);
-
-// data: {
-//     user : null,
-//     comment : null,
-//     messages:[],
-// },
-// methods:{
-//     save(){
-//         const message= {
-//             user:this.user,
-//             comment:this.comment,
-//         };
-//         // 내림차순 unshift
-//         this.messages.unshift(message)
-//         this.user=null
-//         this.comment=null
-//     }
-// },
+});
 
 let app = new Vue ({
     el: '#app',
