0892c588f4c68c3a38f3269b006f4f85e74c72c0
KRHDY <1901322@g.yju.ac.kr>
2021-07-19 16:12:53 +0900

    컴퓨티드 프로퍼티, 그림,updateProduct 함수수정및 컴퓨티드 프로퍼티로 이미지추가 data의 각 variant 객체에 variantQuantity 라는 정수형 변수를 추가 inStock을 Computed property로 변경하는 함수 구현 inStock 을 Computed property로 변경하는 함수 구현

--- excersice/main.js
+++ excersice/main.js
@@ -4,8 +4,9 @@ let app = new Vue ({
     data:{
         brand:'「近藤」',
         product:'靴下' ,
-        image:'./assets/images/socks_blue.jpg',
-        inStock:false,
+        selectedVariant:0,
+        // image:'./assets/images/socks_blue.jpg',
+        // inStock:false,
         // inventory:100,
         details:['80% cotton','20% polyester','Gender-neutral'],
     
@@ -14,11 +15,13 @@ let app = new Vue ({
                 variantId:2334,
                 variantColor:'green',
                 variantImage:'./assets/images/socks_green.jpg',
+                variantQuantity:10,
             },
             {
                 variantId:2335,
                 variantColor:'blue',
                 variantImage:'./assets/images/socks_blue.jpg',
+                variantQuantity:0
             }
         ],
         cart:0,
@@ -28,14 +31,23 @@ let app = new Vue ({
         addToCart(){
             this.cart += 1
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        // updateProduct(variantImage){
+        //     this.image = variantImage
+        // },
+        updateProduct(index){
+            this.selectedVariant = index
         },
     },
 
     computed:{
         title(){
             return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 });
