f9f6a0dde8f1878f7b5ff752a031035172473236
KRHDY <1901322@g.yju.ac.kr>
2021-07-19 16:54:04 +0900

    댓글 달기

--- excersice/중간파일1/main.js
+++ excersice/중간파일1/main.js
@@ -1,3 +1,38 @@
+Vue.component('comments',{
+    
+  template:` <div>
+  <input type="text" v-model="user">
+  <input type="text" v-model="comment">
+  <button @click="save">저장</button><br>
+  <ul>
+      <li v-for="msg in messages">
+          <p>{{ msg.user}} : {{msg.comment}}</p>
+      </li>
+  </ul>
+  </div>`, 
+  data(){
+      return { 
+          user : null,
+          comment : null, 
+          messages:[],
+      }
+  },
+  methods:{
+          save(){
+              const message= {
+                  user:this.user,
+                  comment:this.comment,
+              };
+              // 내림차순 unshift
+              this.messages.unshift(message)
+              this.user=null
+              this.comment=null
+          }
+  }
+});
+
+
+
 Vue.component('product', {
     template: `
      <div class="product">  
