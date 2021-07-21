c2350f00c15538230be1e0eb14725a9fd0d46fde
KRHDY <1901322@g.yju.ac.kr>
2021-07-21 14:35:04 +0900

    조금 바꿈

--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+new Vue ({
+  render:function (createElement) {
+    return createElement(app)
+  }
+}).$mount('#app');
