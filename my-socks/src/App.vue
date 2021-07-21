b745b40e9f2eb963d9e328e7072dac0cc8f9b84b
KRHDY <1901322@g.yju.ac.kr>
2021-07-21 14:59:12 +0900

    양말 추가

---
+++ my-socks/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ProductPage></ProductPage>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import Product from './components/Product.vue'
+import ProductPage from './components/ProductPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    // Product
+    ProductPage
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+@import './assets/css/styles.css'
+</style>
