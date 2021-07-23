69dc4d75c7dc693a1eb2f341442c8550bcf72ef9
KRHDY <1901322@g.yju.ac.kr>
2021-07-23 16:50:36 +0900

    2021년 7월 23일 금요일 16시 50분 36초 KST < commit >

---
+++ my-cafe/src/components/Reviews.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <h1>Reviews</h1>
+        <h1>{{ review_title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Reviews',
+    props:['review_id'],
+    computed:{
+        review_title(){
+            if(this.review_id=='1'){
+                return 'review 1'
+            }else if (this.review_id=='2'){
+                return 'review 2'
+            }else 
+                return ''
+        }
+    }
+};
+</script>
