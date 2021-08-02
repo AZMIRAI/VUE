c7228e06b72d6f719f10520496942560d2d890f9
KRHDY <1901322@g.yju.ac.kr>
2021-08-02 14:52:28 +0900

    19페이지 까지

--- my-cafe/src/components/Home.vue
+++ my-cafe/src/components/Home.vue
@@ -1,12 +1,30 @@
 <template>
-    <!-- <div id="app">
-        <h1>Home</h1>
-    </div> -->
+    <div id="app">
     <h1>Welcome {{$store.state.userId}} </h1>
-</template>
+    <input v-model="newId" type="text"><button @click="updateUserId">Save</button>
+    <br>
+    <button @click = "updateReviews">Update</button>
+    <ul>
+        <li v-for="r in $store.state.reviews" :key="r.id">
+            <p> {{ r.body }}</p>
+        </li>
+    </ul>
+    </div>
+    </template>
 
 <script>
 export default {
     // name:'Home'
-};
+    date(){
+        return { newId:''}
+    },
+    methods:{
+        updateUserId(){
+            this.$store.commit('updateUserId', this.newId)
+        },
+        updateReviews(){
+            this.$store.dispatch('getReviews')
+        }
+    }
+}
 </script>
--- my-cafe/src/store/store.js
+++ my-cafe/src/store/store.js
@@ -1,11 +1,43 @@
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// Vue.use(Vuex);
+
+// export default new Vuex.Store({
+//     state:{
+//         userId:'hello?'
+//     }
+// });
+
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state:{
-        userId:'hello?'
+        userId:'user',
+        reviews:[]
+    },
+    mutations:{
+        updateUserId(state, newId){
+            state.userId = newId;
+        },
+        updateReviews(state, reviews){
+            state.reviews = reviews
+        }
+    },
+    actions:{
+        getReviews({commit}){
+            axios.get('/api/comments')
+            .then(response=>{
+                commit('updateReviews', response.data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
     }
 });
 
