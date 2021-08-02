1943cdc0501774e2f2f3a15987c1d7d593d87b4f
KRHDY <1901322@g.yju.ac.kr>
2021-08-02 14:13:59 +0900

    vue.config,reviews.vue 작성

--- my-cafe/src/components/Reviews.vue
+++ my-cafe/src/components/Reviews.vue
@@ -1,23 +1,49 @@
 <template>
     <div id="app">
         <h1>Reviews</h1>
-        <h1>{{ review_title }}</h1>
+
+        <ul>
+            <li v-for="c in comments" :key="c.id">
+                <p> {{ c.body }} </p>
+            </li>
+        </ul>
+
+        <!-- <h1>{{ review_title }}</h1> -->
     </div>
 </template>
 
 <script>
-export default {
-    name:'Reviews',
-    props:['review_id'],
-    computed:{
-        review_title(){
-            if(this.review_id=='1'){
-                return 'review 1'
-            }else if (this.review_id=='2'){
-                return 'review 2'
-            }else 
-                return ''
-        }
+// export default {
+//     name:'Reviews',
+//     props:['review_id'],
+//     computed:{
+//         review_title(){
+//             if(this.review_id=='1'){
+//                 return 'review 1'
+//             }else if (this.review_id=='2'){
+//                 return 'review 2'
+//             }else 
+//                 return ''
+//         }
+//     }
+// };
+
+import axios from 'axios'
+
+export default{
+    name: 'Reviews',
+    data(){
+        return { comments : [] }
+    },
+    mounted(){
+        axios.get('/api/comments')
+        .then(response=>{
+            console.log(response)
+            this.comments=response.data
+        })
+        .catch(err=>{
+            console.log(err)
+        })
     }
 };
 </script>
