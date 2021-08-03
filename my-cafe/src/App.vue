503aff2860083580379a2153a116a5383019d94f
KRHDY <1901322@g.yju.ac.kr>
2021-08-03 14:09:09 +0900

    router vuew ㅇㅇ

--- my-cafe/src/App.vue
+++ my-cafe/src/App.vue
@@ -39,21 +39,19 @@
 
     <v-main>
       <router-view></router-view>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  // components: {
+  //   HelloWorld,
+  // },
   data: () => ({
     //
   }),
