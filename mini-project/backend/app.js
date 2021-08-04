88d86eb0f98dc8d5ce05c2ad466307febe658d9d
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-04 14:45:47 +0900

    2021년 8월  4일 수요일 14시 45분 47초 KST < app.js 파일에 다음 내용 추가 >

--- mini-project/backend/app.js
+++ mini-project/backend/app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// 이거 추가
+var booksRouter = require('./routes/books');
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// 이거 추가
+app.use('/api/books',booksRouter);
 
 module.exports = app;
