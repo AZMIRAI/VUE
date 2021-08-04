f2de0992a0e3a81c9ca99d0d1d9258091b7929b4
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-04 14:52:11 +0900

    2021년 8월  4일 수요일 14시 52분 11초 KST < routes/books.js 파일 수정 >

--- mini-project/backend/routes/books.js
+++ mini-project/backend/routes/books.js
@@ -1,4 +1,20 @@
 const express   =   require('express');
 const router    =   express.Router();
+const books     =   require('../model/books');
 
 module.exports  =   router;
+
+router.get('/', (req, res) => {
+    let briefs = []
+    books.forEach(element => {
+        let {bookId, title, author, cover} = element
+        briefs.push({bookId, title, author, cover})
+    });
+    res.json(briefs)
+})
+
+router.get('/:bookId', (req,res)=>{
+    const bookId = req.params.bookId
+    let book = books.find(element=> elment.bookId==bookId)
+    res.json(book)
+})
