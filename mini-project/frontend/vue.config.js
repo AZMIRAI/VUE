46933542d543f222e0773b7ac538b543634440f5
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-04 14:33:18 +0900

    2021년 8월  4일 수요일 14시 33분 18초 KST < frontend 폴더에 vue.config.js파일을 생성 >

--- mini-project/frontend/vue.config.js
+++ mini-project/frontend/vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  devServer:{
+    // /api로 시작되는 요청 주소 정의
+
+    proxy:{
+      '/api' : {
+        target: 'http://localhost:3000/api',
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    }
+  };
+  
+  
+  
   transpileDependencies: [
     'vuetify'
   ]
