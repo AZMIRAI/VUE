e25fdf3c8d531610049d912a0676dc4cde9dbb29
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-05 14:34:34 +0900

    router/index.js 수정

--- my-memo/my-memo/src/router/index.js
+++ my-memo/my-memo/src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Add from '../views/AddMemo.vue'
+import Read from '../views/ReadMemo.vue'
+import Signin from '../views/Signin'
+import Signup from '../views/Signup'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/' ,name: 'Home',component: Home },
+  { path: '/add' ,name: 'Add',component: Add },
+  { path: '/memos/:memoId' ,name: 'Read',component: Read },
+  { path: '/signin' ,name: 'Signin',component: Signin },
+  { path: '/signup' ,name: 'Signup',component: Signup }, 
 ]
 
 const router = new VueRouter({
