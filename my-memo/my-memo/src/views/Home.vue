7e1de8ed1ec95c859e4587aa83c11e5e1e01d6b9
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-05 14:46:33 +0900

    컴포넌트 내부가드, 컴포넌트에서 체크

--- my-memo/my-memo/src/views/Home.vue
+++ my-memo/my-memo/src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <p v-if="requiresAuth">
+      <router-link :to="{path:'/signin'}">Signin</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
-
   export default {
     name: 'Home',
-
+    data(){
+      return {
+        requiresAuth:false
+      }
+    },
+    beforeRouteEnter(to,from,next){
+      next(vm=>{
+        vm.requiresAuth=true
+      })
+    },
+    beforRouteLeave(to,from,next){
+      this.requiresAuth=false
+      next()
+    }
   }
 </script>
